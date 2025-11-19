b82575860f6956be64e5bc1ed6e480b76cca90fc
Arthur Wagenknecht <oarthurdev@gmail.com>
2025-11-19 14:02:05 -0300

    changes

--- mobile/app/cliente/_layout.js
+++ mobile/app/cliente/_layout.js
@@ -56,6 +56,12 @@ export default function ClienteLayout() {
           headerShown: false,
         }} 
       />
+      <Stack.Screen 
+        name="avaliar/[id]" 
+        options={{ 
+          headerShown: false,
+        }} 
+      />
     </Stack>
   );
 }
